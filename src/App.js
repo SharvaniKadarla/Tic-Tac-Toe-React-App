c7ac68e192c7a3cda9e40ed36ae63415073f1d37
Sharvani Kadarla <sk10362n@pace.edu>
2025-10-01 20:06:24 -0400

    fix: prevent overwriting already filled squares by returning early in handleClick

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+function Square({value, onSquareClick}) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -13,15 +13,19 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
-      nextSquares[i] = "X";
+      nextSquares[i] = 'X';
     } else {
-      nextSquares[i] = "O";
+      nextSquares[i] = 'O';
     }
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+
   return (
     <>
       <div className="board-row">
@@ -41,4 +45,4 @@ export default function Board() {
       </div>
     </>
   );
-};
+}
